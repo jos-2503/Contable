1daf35e25f17f26c61578e9b94a5a46b481c7f7b
Jose Rafael Torres Calderon <jose.torresc@coppel.com>
2023-07-26 23:54:55 -0700

    se actualiza texto de layaout

--- app/src/main/java/com/example/contable/MainActivity.kt
+++ app/src/main/java/com/example/contable/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Jose Rafael, Bienvenido")
+                    Greeting("App contable personal")
                 }
             }
         }
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = name,
         modifier = modifier
     )
 }
